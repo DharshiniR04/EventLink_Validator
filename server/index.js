c26c72bf3998dd9c67f424e55c4549668c7a1552
Dharshini R <114658091+DharshiniR04@users.noreply.github.com>
2024-10-21 18:25:55 +0530

    Updated Client URL

--- server/index.js
+++ server/index.js
@@ -11,12 +11,12 @@ connectDB();
 
 const app = express();
 app.use(cors({
-    origin: "https://password-changer-client.vercel.app", 
+    origin: "https://event-link-validator-client.vercel.app", 
     methods: ["POST", "GET","PATCH"],
     credentials: true
   }));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://password-changer-client.vercel.app"); 
+    res.header("Access-Control-Allow-Origin", "https://event-link-validator-client.vercel.app"); 
     res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.header("Access-Control-Allow-Credentials", "true");
