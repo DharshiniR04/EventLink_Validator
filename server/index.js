4051bfed2abf8a0724933e9832dcb935f410f383
Dharshini R <114658091+DharshiniR04@users.noreply.github.com>
2024-11-02 10:24:49 +0530

    Updated Limitation MB for file

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   });
 app.use(cors());
 app.use(express.json());
-app.use(express.json({ limit: '20mb' }));
+app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 app.get("/",async(req,res)=>{
